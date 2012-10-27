991aac62e17fb10c2f9f9e60f0204f9067319205
@deniszgonjanin <deniszgonjanin@gmail.com>
2012-10-27 16:26:25 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+JanesWalk Ottawa Community website
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Janeswalkottawa::Application.config.secret_token = 'd82f48bd2107d87bc4eaefc35bd468f28c87caddc252346224800dd547137d08037050e9466fd1ac2fb220cddf06113a3c977a20d25e35b5e1f9e1a23c23f9c4'
