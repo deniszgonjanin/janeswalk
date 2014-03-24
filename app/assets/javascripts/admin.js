343a62b657c2dfd08b42b4d687f76fef0a48161e
Denis Zgonjanin <deniszgonjanin@gmail.com>
2014-03-24 11:23:57 -0400

    image outline and image caption styles

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -10,7 +10,14 @@ if (typeof(custom_wymeditor_boot_options) == "undefined") {
       , {'name': 'h3', 'title':'Heading_3', 'css':'wym_containers_h3'}
       , {'name': 'h4', 'title':'Heading_4', 'css':'wym_containers_h4'}
       , {'name': 'p', 'title':'Paragraph', 'css':'wym_containers_p'}
-    ]
+    ],
+    classesItems: [
+		{name: 'text-align', rules:[{name: 'left', title: '{Left}'}, {name: 'center', title: '{Center}'}, {name: 'right', title: '{Right}'}, {name: 'justify', title: '{Justify}'}], join: '-', title: '{Text_Align}'}
+		, {name: 'image-align', rules:[{name: 'left', title: '{Left}'}, {name: 'right', title: '{Right}'}], join: '-', title: '{Image_Align}'}
+		, {name: 'font-size', rules:[{name: 'small', title: '{Small}'}, {name: 'normal', title: '{Normal}'}, {name: 'large', title: '{Large}'}], join: '-', title: '{Font_Size}'}
+		, {name: 'image', rules:[{name: 'outline', title: 'Outline'}, {name: 'caption', title: 'Caption'}], join: '-', title: 'Image Options'}
+		]
+
   }; 
 }
 
